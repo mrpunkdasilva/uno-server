d72d8f603f53c9f3d139e3c330e5cdcca7fb1c09
Gustavo Henrique de Jesus da Silva <gustavo.jesus0724@jala.university>
2026-01-31 23:48:10 -0300

    refactor: Update router to use named export for auth middleware

--- src/presentation/routes/index.js
+++ src/presentation/routes/index.js
@@ -5,13 +5,13 @@ import gameRoutes from './game.routes.js';
 import scoreRoutes from './score.routes.js';
 import authRoutes from './auth.routes.js';
 
-import authentication from '../middlewares/authentication.middleware.js';
+import { authenticateToken } from '../middlewares/authentication.middleware.js'; // Import authenticateToken directly
 
 const router = Router();
 
 router.use('/api/auth', authRoutes);
-router.use('/api/players', authentication, playerRoutes);
-router.use('/api/games', authentication, gameRoutes);
-router.use('/api/scores', authentication, scoreRoutes);
+router.use('/api/players', authenticateToken, playerRoutes); // Use authenticateToken directly
+router.use('/api/games', authenticateToken, gameRoutes); // Use authenticateToken directly
+router.use('/api/scores', authenticateToken, scoreRoutes); // Use authenticateToken directly
 
 export default router;
