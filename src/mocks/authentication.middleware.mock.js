bb2a2cde8bd3bebe9582dd50be462daf59ad6bf3
Gustavo Henrique de Jesus da Silva <gustavo.jesus0724@jala.university>
2026-01-31 23:48:32 -0300

    fix(mocks): Remove unnecessary comment from auth mock

--- src/mocks/authentication.middleware.mock.js
+++ src/mocks/authentication.middleware.mock.js
@@ -1,7 +1,6 @@
 import { jest } from '@jest/globals';
 
-// A instância do mock que pode ser acessada e configurada
 export const mockAuthenticateToken = jest.fn((req, res, next) => {
-  req.user = { id: 'initialMockUserId' }; // Default value, can be overridden
+  req.user = { id: 'initialMockUserId' };
   next();
 });
