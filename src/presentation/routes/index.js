19f33d02c13974981c27e1b0e41bdaf88de44a25
Gustavo Henrique de Jesus da Silva <gustavo.jesus0724@jala.university>
2026-02-03 11:31:31 -0300

    chore: remove unecessary comment

--- src/presentation/routes/index.js
+++ src/presentation/routes/index.js
@@ -11,9 +11,9 @@ import { authenticateToken } from '../middlewares/authentication.middleware.js';
 const router = Router();
 
 router.use('/api/auth', authRoutes);
-router.use('/api/players', authenticateToken, playerRoutes); // Use authenticateToken directly
-router.use('/api/games', authenticateToken, gameRoutes); // Use authenticateToken directly
-router.use('/api/scores', authenticateToken, scoreRoutes); // Use authenticateToken directly
+router.use('/api/players', authenticateToken, playerRoutes);
+router.use('/api/games', authenticateToken, gameRoutes);
+router.use('/api/scores', authenticateToken, scoreRoutes);
 router.use('/api/cards', authenticateToken, cardroutes);
 
 export default router;
