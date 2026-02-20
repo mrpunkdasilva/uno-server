507bfae552dff1e36545db34917da15679ae1d84
Kádymo <santanakadymo@gmail.com>
2026-02-20 01:19:36 -0300

    feat: Using memoization middleware with default config

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
-import redisClient from './config/redis.js';
 import connectDB from './config/database.js';
+import redisClient from './config/redis.js';
 
-import Router from './presentation/routes/index.js';
-import logger from './config/logger.js';
 import pinoHttp from 'pino-http';
-import errorMiddleware from './presentation/middlewares/error.middleware.js'; // ✅ ADICIONAR
+import { defaultCacheConfig } from './config/cache.js';
+import logger from './config/logger.js';
+import errorMiddleware from './presentation/middlewares/error.middleware.js';
+import memoizationMiddleware from './presentation/middlewares/memoization.middleware.js';
+import Router from './presentation/routes/index.js';
 
 if (process.env.NODE_ENV !== 'test') {
   connectDB();
@@ -18,9 +20,10 @@ const app = express();
 app.use(pinoHttp({ logger }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(memoizationMiddleware(defaultCacheConfig));
 app.use(Router);
 
-// ✅ IMPORTANTE: middleware de erro sempre depois das rotas
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
