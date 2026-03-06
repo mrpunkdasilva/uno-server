631c284163ae5fe4fb26b78cbe9a0c36a9ac1047
Gustavo Henrique de Jesus da Silva <gustavo.jesus0724@jala.university>
2026-03-06 10:26:05 -0300

    add new beans

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,3 +1,11 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' }, modules: false }]],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: { node: 'current' },
+        modules: process.env.NODE_OPTIONS?.includes('--experimental-vm-modules') ? false : 'auto',
+      },
+    ],
+  ],
 };
